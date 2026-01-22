ce9f42380eab972c9a0709418c37aa896a5336f2
IanChains <33020@edu.campusdetandem.be>
2026-01-22 22:45:14 +0100

    Add logging for database connection status
    Replaced print statements with logging in main.py to provide better visibility into database connection status. Updated requirements.txt to include the logging dependency.

--- src/API/unsafe/app/main.py
+++ src/API/unsafe/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from .config import settings
 import asyncpg
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -10,15 +14,15 @@ async def startup_db_test():
    dsn = f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
    try:
       conn = await asyncpg.connect(dsn)
-      print("DB connection successful")
+      logger.info("DB connection successful")
       await conn.close()
    except Exception as e:
-      print("DB connection failed:", e)
+      logger.critical("DB connection failed: %s", e)
 
 
 @app.get("/")
 def first_example():
-  '''
-  FG Example First Fast API Example 
-  '''
-  return {"GFG Example": "FastAPI"}
+   '''
+   FG Example First Fast API Example 
+   '''
+   return {"GFG Example": "FastAPI"}
