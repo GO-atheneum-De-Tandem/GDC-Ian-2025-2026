1ab43cbdc5c48cc0e7359ac099097959667deb39
IanChains <33020@edu.campusdetandem.be>
2026-01-22 22:48:29 +0100

    Switch env_file to Docker setting in config files
    Changed the env_file path in both safe and unsafe app config.py files to use the Docker setting by default. This update ensures environment variables are loaded from .env in the current directory, aligning with Docker deployment.

--- src/API/safe/app/config.py
+++ src/API/safe/app/config.py
@@ -8,8 +8,8 @@ class Settings(BaseSettings):
     DB_NAME: str
 
     class Config:
-        #env_file = ".env" #docker setting
-        env_file = "../.env" #local setting
+        env_file = ".env" #docker setting
+        #env_file = "../.env" #local setting
 
 settings = Settings()
 # gebruik settings.DB_HOST etc.
